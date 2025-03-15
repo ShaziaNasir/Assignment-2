13a567a67697b09ab8c52835d00f933b24091817
ShaziaNasir <148135973+ShaziaNasir@users.noreply.github.com>
2025-03-15 23:55:06 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,101 @@
+
+# print("ASSIGNMENT #2")
+
+# print("PYTHON OPERATORS")
+
+# print("PYTHON ARITHMETIC OPERATORS")
+# x=5
+# y=7
+
+# print("1.Addition operators")
+# print(x+y)
+
+# print("2.Subtraction operators")
+# print(x-y)
+
+# print("3.Multiplication operators")
+# print(x*y)
+
+# print("4.division operators")
+# print(x/y)
+
+# print("5.Modulus")
+# print(y%x)
+
+# print("6.Floor division")
+# print(y//x)
+
+# print("7.Exponent")
+# print(x **3)
+
+# print("ASSIGNMENT OPERATORS")
+# #Simple assignment
+# x=15
+# print("x=",x)
+# #Add & asign
+# x+=5
+# print("x after +=5:",x)
+# #Subtract assign
+# x-=5
+# print("x after -=5:",x)
+# #Multiple & assign
+# x*=5
+# print("x after *=5:",x)
+# #Divide assign
+# x/=3
+# print("x after /=3:",x)
+# #Modulus assign
+# x%=5
+# print("x after%=5:",x)
+# # Floor assign
+# x//=5
+# print("x after//=5:",x)
+# # exponent assign
+# x**= 2
+# print("x after**=2:",x)
+
+# print("COMPARISION OPERATORS")
+# x=5
+# y=3
+# print(x==y)    #equal to
+# print(x!=y)     #not equal to
+# print(x>y)      #greater than
+# print(x<y)      #less than
+# print(x>=y)     #greater than or equal to
+# print (x>=y)    #less than or equal to
+
+# print("logical operators")
+# x=15
+# print(x>10 and x<20)       #and
+# print(x>10 or x<20)        #or
+# print (not(x>10 and x<20))  # not
+
+# print("Membership operators")
+# fruits:list[str]=["banana","mango","apple","cherry"]
+# result="banana" in fruits
+# print(result)
+# survey="mango"not in fruits
+# print(survey)
+
+print("Bitwise operators")
+# Bit wise And
+a=0b1010
+b=0b1100
+c=a&b
+print(bin(c))
+#Bit wise OR
+d=a | b
+print(bin(d))
+#BiTwise XOR
+e=a^b
+print(bin(e))
+#Bit wise NOT
+f=~a
+print(bin(f))
+#Bitwise left shift
+g=b<<2
+print(bin(g))
+#Bit wise Rigt shift
+h=b>>1
+print(bin(h))
+
